6054d50652d26aa4f5fb2bebc736b12644701426
Arham Khan <apple@apples-Mac-mini.local>
2026-08-01 19:56:04 +0600

    Books api done. Starting authors.

---
+++ src/main/java/com/example/testing/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.example.testing.repository;
+
+import com.example.testing.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {}
