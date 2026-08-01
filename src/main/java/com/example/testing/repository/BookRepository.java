8f81a0156c31c1891a5361dd44c5e611e1d5b556
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2026-08-01 20:15:12 +0600

    First Commit

--- src/main/java/com/example/testing/repository/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.testing.repository;
-
-import com.example.testing.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {}
