48ab35fbc2afbc48e578a7ebff2c1d33f1403131
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2026-08-01 20:16:36 +0600

    Books CRUD done. Starting Authors.

---
+++ src/main/java/com/example/testing/controller/BookController.java
@@ -0,0 +1,45 @@
+package com.example.testing.controller;
+
+import com.example.testing.entity.Book;
+import com.example.testing.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService service;
+
+    public BookController(BookService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return service.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable Long id) {
+        return service.getBook(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return service.create(book);
+    }
+
+    @PatchMapping("/{id}")
+    public Book updateBook(@PathVariable Long id,
+                           @RequestBody Book book) {
+
+        return service.update(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
