bdaa4ef0de1f277ef2e03d5bfb95a60349567551
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2026-08-03 16:24:13 +0600

    Flyway works. Moving towards manual sync.

---
+++ src/main/resources/db/migration/V3__seed_authors.sql
@@ -0,0 +1,5 @@
+INSERT INTO author (name, email)
+VALUES
+    ('George Orwell', 'orwell@example.com'),
+    ('J.K. Rowling', 'jkrowling@example.com'),
+    ('J.R.R. Tolkien', 'tolkien@example.com');
---
+++ src/main/resources/db/migration/V4__seed_books.sql
@@ -0,0 +1,5 @@
+INSERT INTO book(title, price, author_id)
+VALUES
+    ('1984', 19.99, 5),
+    ('Harry Potter', 29.99, 6),
+    ('The Hobbit', 24.99, 7);
